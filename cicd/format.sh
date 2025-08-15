75e2c763d44da0bfc9de91c6e0275e9304f50598
Will Hawkins <hawkinsw@obs.cr>
2025-08-15 08:45:30 -0400

    Add clang-format As Github Workflow
    Signed-off-by: Will Hawkins <hawkinsw@obs.cr>

--- cicd/format.sh
+++ cicd/format.sh
@@ -1,3 +1,14 @@
 #!/bin/env bash
 
-clang-format -i --files=$1
+# TODO: Error checking
+
+check_command=""
+if [ $1 = "format" ]; then
+	echo "Formatting in place ..."
+	check_command="-i"
+else
+	echo "Checking only ..."
+	check_command="--dry-run -Werror"
+fi
+
+clang-format --verbose ${check_command} --files=$2
